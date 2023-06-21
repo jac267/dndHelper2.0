836bd5add5481c90156ac7195d158325c3fde964
jac267 <jackpotchap@gmail.com>
2023-06-21 17:05:45 -0400

    mouvable caracter

--- choosingACaracter.js
+++ choosingACaracter.js
@@ -1,3 +1,4 @@
+caracter = { id: "", init: "" };
 function generateCaracterList(ls_token) {
   document.getElementById("ls-caracter").innerHTML = "";
   for (const e of ls_token) {
@@ -9,13 +10,26 @@ function aCaracter(token) {
   output_li = document.createElement("li");
   output_img = document.createElement("div");
   output_p = document.createElement("p");
+  output_p2 = document.createElement("p");
 
   output_li.className = "caracter";
 
+  output_li.onclick = () => confirmeCaracter(token.id, token.stats.Init);
   output_img.style.backgroundImage = token.picture;
   output_p.textContent = token.stats.Init;
+  output_p2.textContent = token.id;
   output_li.appendChild(output_img);
   output_li.appendChild(output_p);
+  output_li.appendChild(output_p2);
 
   return output_li;
+}
+
+function confirmeCaracter(id, init) {
+  if (confirm("Vous avez selectionner: " + id)) {
+    caracter.id = id;
+    caracter.init = init;
+    document.getElementsByClassName("choose-your-caracter")[0].style.display =
+      "none";
+  }
 }
--- server.js
+++ server.js
@@ -4,13 +4,40 @@ const fs = require("fs");
 app.use(express.json({ limit: "50mb" }));
 
 mapping = {};
-
+mouvement = [];
 app.use(express.static(__dirname));
 app.get("/mapping", function (req, res) {
   res.send("mappingDict = " + JSON.stringify(mapping));
 });
 app.post("/", function (request, response) {
   mapping = request.body;
+
+  response.send({ message: "No bitches Found" }); // echo the result back
+});
+app.get("/mouvementUpdate", function (req, res) {
+  res.send("ls_mouvement = " + JSON.stringify(mouvement));
+});
+
+app.post("/move", function (request, response) {
+  mouvement.push({
+    id: request.body.id,
+    x: request.body.x,
+    y: request.body.y,
+    trasactionId: String(Date.now()) + request.body.id,
+  });
+
+  response.send(request.body); // echo the result back
+});
+
+app.post("/moveReceive", function (request, response) {
+  for (const e of mouvement) {
+    for (const id of request.body.toRemove) {
+      if (e.trasactionId == id) {
+        index = mouvement.indexOf(e);
+        mouvement.splice(index, 1);
+      }
+    }
+  }
 
   response.send(request.body); // echo the result back
 });
